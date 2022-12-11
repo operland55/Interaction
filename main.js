71efbaa0381128a8ef525bd2dc1b5f9f6919a782
jungpa <operland55@naver.com>
2022-12-11 18:42:27 +0900

    ✅ Reflecting the current lively scene

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 (() => {
+	let yOffset = 0; //widow.scrollY 변수
+	let prevScrollHeight = 0; // 현재 스크롤 위치보다 이전에 위치한 스크롤 섹션들의 높이값의 핪
+	let currentScene = 0; //현재 활성화된(눈 앞에 보고있는 씬)
+
 	const sceneInfo = [
 		{
 			// 0
@@ -46,10 +50,40 @@
 				i
 			].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
 		}
-		console.log(sceneInfo);
+
+		yOffset = window.scrollY;
+		let totalScrollHeight = 0;
+		for (let i = 0; i < sceneInfo.length; i++) {
+			totalScrollHeight += sceneInfo[i].scrollHeight;
+			if (totalScrollHeight >= yOffset) {
+				currentScene = i;
+				break;
+			}
+		}
+		document.body.setAttribute("id", `show-scene-${currentScene}`);
 	}
 
-	window.addEventListener("resize", setLayout);
+	function scrollLoop() {
+		prevScrollHeight = 0;
+		for (let i = 0; i < currentScene; i++) {
+			prevScrollHeight += sceneInfo[i].scrollHeight;
+		}
+		if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+			currentScene++;
+			document.body.setAttribute("id", `show-scene-${currentScene}`);
+		}
+		if (yOffset < prevScrollHeight) {
+			if (currentScene == 0) return; //브라우저 바운스 효과로 인해 마이너스가 되는것을 방지(모바일)
+			currentScene--;
+			document.body.setAttribute("id", `show-scene-${currentScene}`);
+		}
+	}
 
-	setLayout();
+	window.addEventListener("scroll", () => {
+		yOffset = window.scrollY;
+		scrollLoop();
+	});
+
+	window.addEventListener("resize", setLayout);
+	window.addEventListener("load", setLayout);
 })();
